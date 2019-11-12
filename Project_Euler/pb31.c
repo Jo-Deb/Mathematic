0231add5e0748a2a2f6e94c5878164eb1e9208ce
Joël Debrais <SoPla@MacBook-Pro-de-Joel.local>
2019-11-12 07:04:13 +0100

    to save

--- Project_Euler/pb31.c
+++ Project_Euler/pb31.c
@@ -6,35 +6,15 @@
  *combinaisons nécessaires à la résolution du pb31
  */
 
-int generationCombi(int longueur) {
-	int tab[7] = {1, 2, 5, 10, 20, 50, 100}, tmp;
-	
-	//Première boucle pour faire avancer dans les combinaisons
-	for (int i = 0; i < 7-longueur; i++){
-		liste * l =  NULL;
-		//Construction complète de la première combinaison
-		//for(int k = i; k <= i+longueur-1; k++){ l = ajoutEnQueue(l, tab[k]);}
-		//Curseur indiquant la valeur "pivot"
-		for( int j = i+longueur-1; j <= 0; j--){
-			tmp = i;
-			while(tab[j] >= tab[tmp] && tmp <= 6){++tmp;}
-			printf("\n");
-			afficheListe(l);
-			l = supprimeFin(l, tab[j]);
-		}
-	}
-}
-
-
-void generefin(liste * l, int ind, int longueur, int combi){
+void generefin(liste * l, int ind, int combi){
 	int tab[7] = {1, 2, 5, 10, 20, 50, 100};
-	int mesure = taille(l); int limit = longueur-combi+mesure;
+	int mesure = taille(l); int limit = 7-combi+mesure;
 	int to_complete = combi-mesure-1;
 
 	for(int i = ind; i <= limit; i++){
 		for(int j = i; j <=6 && j <= i+to_complete; j++){l = ajoutEnQueue(l, tab[j]);}
-		for(int k = j; k < 6; k++){
-			if(k > j){l = ajoutEnQueue(l, tab[k]);}
+		for(int k = i+to_complete; k < 6; k++){
+			if(k > i+to_complete){l = ajoutEnQueue(l, tab[k]);}
 			printf("\n");
 			afficheListe(l);
 			l = supprElt(l, tab[k]);
@@ -42,9 +22,32 @@ void generefin(liste * l, int ind, int longueur, int combi){
 	}
 }
 
+int generationCombi(int longueur) {
+	int tab[7] = {1, 2, 5, 10, 20, 50, 100}, tmp;
+	
+	//Première boucle pour faire avancer dans les combinaisons
+	for (int i = 0; i < 7-longueur; i++){
+		liste * l =  NULL;
+		//Construction complète de la première combinaison
+		for(int k = i; k <= i+longueur-1; k++){ l = ajoutEnQueue(l, tab[k]);}
+		//Curseur indiquant la valeur "pivot"
+		for( int j = i+longueur-1; j <= 0; j--){
+			tmp = i;
+			while(tab[j] >= tab[tmp] && tmp <= 6){++tmp;}
+			printf("\n");
+			afficheListe(l);
+			l = supprimeFin(l, tab[j]);
+			generefin(l, tmp, longueur);
+		}
+	}
+	return 0;
+}
+
+
 int main(){
 	int tab[7] = {1, 2, 5, 10, 20, 50, 100};
-	int t1 = 0, t2 = 0, pas = 2, tmp, limit = 7;
+	generationCombi(2);
+	/*int t1 = 0, t2 = 0, pas = 2, tmp, limit = 7;
 
 	while(t1 <= 7-1-pas){
 		liste * l = NULL;
@@ -67,5 +70,6 @@ int main(){
 		supprimeListe(l);
 	}
 	printf("\n");
+	*/
 	return 0;
 }
