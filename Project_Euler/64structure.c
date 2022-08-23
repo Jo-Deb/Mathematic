3addf9dc08d10de907f33058842fe0d6916ed82a
Jojo <joel.debrais-ext@rexel.fr>
2022-08-23 13:48:59 +0200

    Avancé sur le 64

--- Project_Euler/64structure.c
+++ Project_Euler/64structure.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "64structure.h"
 
-irractionnel * createIrrationnel(int racine, int suppl){
+irrationnel * createIrrationnel(int racine, int suppl){
 	irrationnel * res = malloc(sizeof(irrationnel));
 	res->racine = racine;
 	res->suppl = suppl;
@@ -23,18 +23,18 @@ cfrac * createCFrac(int an, fractir * frac){
 	return res;
 }
 
-int egaliteIrrationnel(irrationnel a, irrationnel b){
-	if (a.racine == b.racine && a.suppl == b.suppl){ return 0;}
+int egaliteIrrationnel(irrationnel * a, irrationnel * b){
+	if (a->racine == b->racine && a->suppl == b->suppl){ return 0;}
 	else return 1;
 }
 
-int egaliteFractir(fractir a, fractir b){
-	if (egaliteIrrationnel(a.num, b.num) == 0 && a.deno == b.deno){ return 0;}
+int egaliteFractir(fractir * a, fractir * b){
+	if (egaliteIrrationnel(a->num, b->num) == 0 && a->deno == b->deno){ return 0;}
 	else return 1;
 }
 
-int egaliteCfrac(cfrac a, cfrac b){
-	if (egaliteFractir(a.frac, b.frac) == 0 && a.an == b.an){ return 0;}
+int egaliteCfrac(cfrac * a, cfrac * b){
+	if (egaliteFractir(a->frac, b->frac) == 0 && a->an == b->an){ return 0;}
 	else return 1;
 }
 
@@ -50,11 +50,58 @@ void freecFrac(cfrac * a){
 	free(a);
 }
 
-int nearestRoot(irrationel * a){
+int nearestRoot(irrationnel * a){
 	int i = 1;
 	while ((i*i) <= a->racine){++i;}
 	--i;
 	return i;
 }
 
-/*Il faut faire la suite du programme*/
+fractir * irraToFractir(irrationnel * a){ return createFractir(a, 1); }
+
+cfrac * fractirToCfrac(fractir * a){return createCFrac(0, a);}
+
+fraction * createFraction(cfrac * num, cfrac * deno){
+    fraction * res = malloc(sizeof(fraction));
+    if(num != NULL){ res->num = num;} else{res->num = NULL;}
+    if(deno != NULL){ res->deno = deno;} else{res->deno = NULL;}
+    return res;
+}
+
+/*___________Fonctions d'affichage______________________*/
+void afficheIrrationnel(irrationnel * a){
+    if(a->racine < 0){
+        printf("%d < 0 erreur pour une racine, le programme échouera", a->racine);
+        return;
+    }
+    if(a->racine > 0 && a->suppl > 0){
+        printf("sqrt(%d)+%d", a->racine, a->suppl);
+        return;
+    }
+    if(a->racine > 0 && a->suppl < 0){
+        printf("sqrt(%d) %d", a->racine, a->suppl);
+        return;
+    }
+    if(a->racine > 0 && a->suppl == 0){
+        printf("sqrt(%d) ", a->racine);
+        return;
+    }
+    if(a->racine == 0 ){
+        printf("%d", a->suppl);
+        return;
+    }
+}
+
+void afficheFractir(fractir * b){
+   irrationnel * a = b->num;
+   printf("(");
+   afficheIrrationnel(a);
+   printf(") / (%d)", b->deno);
+}
+
+void afficheCfrac(cfrac * c){
+    printf("%d + (", c->an);
+    afficheFractir(c->frac);
+    printf(" )");
+}
+
--- Project_Euler/64structure.h
+++ Project_Euler/64structure.h
@@ -3,32 +3,45 @@
 struct irrationnel {
 	int racine;
 	int suppl;
-} 
+}; 
 typedef struct irrationnel irrationnel;
 
 struct fractir {
 	irrationnel * num;
  	int 	deno;
-}
+};
 typedef struct fractir fractir;
 
 struct fractionContinue {
 	int an;
 	fractir * frac;
-}
+};
 typedef struct fractionContinue cfrac;
 
-/*Définition des fonctions pour l'utilisation des types*/
+struct fraction {
+    cfrac * num;
+    cfrac * deno;
+};
+typedef struct fraction fraction;
 
-irrationnel * createIrrationnel(int racine, int suppl);
-fractir * createFractir(irrationnel * num, int deno);
-cfrac * createCFrac(int an, fractir * frac);
-int egaliteIrrationnel(irrationnel a, irrationnel b);
-int egaliteFractir(fractir a, fractir b);
-int egaliteCfrac(cfrac a, cfrac b);
-void freeIrrationnel(irrationnel * a);
-void freeFractir(fractir * a);
-void freecFrac(cfrac * a);
-int nearestRoot(irrationnel * a);
-fractir * irraToFractir(irrationnel * a);
-cfrac * fractirToCfrac(fractir * a);
+/*Définition des fonctions pour l'utilisation des types*/
+/*________fonctions d'initialisation des types_______________*/
+extern irrationnel * createIrrationnel(int racine, int suppl);
+extern fractir * createFractir(irrationnel * num, int deno);
+extern cfrac * createCFrac(int an, fractir * frac);
+extern int egaliteIrrationnel(irrationnel * a, irrationnel * b);
+extern int egaliteFractir(fractir * a, fractir * b);
+extern int egaliteCfrac(cfrac * a, cfrac b);
+extern void freeIrrationnel(irrationnel * a);
+extern void freeFractir(fractir * a);
+extern void freecFrac(cfrac * a);
+extern int nearestRoot(irrationnel * a);
+extern fractir * irraToFractir(irrationnel * a);
+extern cfrac * fractirToCfrac(fractir * a);
+extern fraction * createFraction(cfrac * num, cfrac * deno);
+/*____________________________________________________________*/
+
+/*___________fonctions d'affichage des structures_____________*/
+extern void afficheIrrationnel(irrationnel * a);
+extern void afficheFractir(fractir * b);
+extern void afficheCfrac(cfrac * c);
