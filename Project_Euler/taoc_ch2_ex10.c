c2523028e2e9d67df7bf2d2d6db6fd6771347d23
Jojo <joel.debrais-ext@rexel.fr>
2023-01-18 16:13:56 +0100

    ça marche

--- Project_Euler/taoc_ch2_ex10.c
+++ Project_Euler/taoc_ch2_ex10.c
@@ -99,7 +99,7 @@ fraction * somme(int n){
 
 int main(){
     int k;
-    for(k=1; k<=10; k++){
+    for(k=1; k<=15; k++){
         printf("cube(%d) = %d\n", k, cube(k));
         //affiche_terme(clcl_terme(k));
         printf("\n");
