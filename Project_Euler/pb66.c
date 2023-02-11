b2f4b913db62af4d4df4bd2a3ce85e89c815711d
Jojo <joel.debrais-ext@rexel.fr>
2023-02-11 20:44:53 +0100

    préservation des versions pour relecture au besoin

---
+++ Project_Euler/pb66.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "liste.h"
+
+unsigned long int CARRE[100000], PREMIER[50000];
+
+void fillCarre(){
+    printf("début de fillCarre\n");
+    int i;
+    for(i=0; i<100000; ++i){ CARRE[i]=(i+1)*(i+1); }
+    printf("fin de fillCarre\n");
+}
+
+void fillPremier(){
+   int i = 0, tmp = 0;
+   FILE * fd = fopen("base_nbr_premiers.dat", "r");
+   while(fscanf(fd, "%d", &tmp)!=EOF && i < 50000){PREMIER[i]=tmp; ++i;}
+}
+
+liste * facteurPremier(int a){
+    liste * res = NULL;
+    int i;
+    for(i=0; (PREMIER[i]*PREMIER[i]) <= a; ++i){
+        if( a % PREMIER[i] == 0 ){ res = ajoutEnTete(res, PREMIER[i]); }
+    }
+    return res;
+}
+
+//retourne a s'il est carré parmis les 100000 premiers carrés
+long int estCarre(long int a){
+    printf("calcul pour %ld\n", a);
+    long int prec = 0, next = 99999, tmp, old_prec, old_next;
+    while(next - prec > 1){
+        while(CARRE[prec] < a && CARRE[next] > a && next - prec > 1){ 
+            old_prec = prec, old_next = next;
+            next -= (next - prec)/2; prec +=(next - prec)/2;
+            printf("prec = %ld et next = %ld, CARRE[prec] = %lu, CARRE[next] = %lu\n", prec, next, CARRE[prec], CARRE[next]);
+        }
+        if(CARRE[prec] > a && CARRE[next] > a){
+            next = prec; prec = old_prec;
+            printf("prec = %ld et next = %ld\n", prec, next);
+        }
+        while(CARRE[prec] < a && CARRE[next] < a){
+            //tmp = next; next += (next - prec)/2; prec = tmp;
+            tmp = next; next += (next - prec); prec = tmp;
+            printf("prec = %ld et next = %ld\n", prec, next);
+        }
+        if(CARRE[prec] == a || CARRE[next] == a){ return a; }
+    }
+    return 0;
+}
+
+int main(){
+    fillCarre();
+    long int i;
+    //for(i=0; i <= 99999; ++i){printf("CARRE[%ld] = %ld\n", i, CARRE[i]);}
+    char o[20] = "n'est pas un carré", n[13] = "est un carré";
+    for(i = 1; i<10000; ++i){
+        printf(" %ld %ld\n", i, estCarre(i));
+    }
+    return 0;
+}
