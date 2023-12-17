e7f67edfcf7154b77ebcc7b6bd46b391ecf45efc
Jojo <joel.debrais-ext@rexel.fr>
2023-12-17 12:48:14 +0100

    68 start

--- Project_Euler/pb68.c
+++ Project_Euler/pb68.c
@@ -49,12 +49,15 @@ int stringSize(char * tab){
  * si la fonction retourne 1, la sous-chaine est présente, 0 sinon*/
 int test_sous_chaine(char * sc, char * st){
     int i, j, tsc = stringSize(sc), tst = stringSize(st), flag = 0;
+
     for(i=0; i < tst; ++i){
         if(flag == 1){return 1;}
         for(j = 0; j < tsc; ++j){
+            /*test pour identifier 1*/
             if(sc[j] == st[i+j] && sc[j]=='1' && tsc == 1 && st[i+j+1] != '0'){flag = 1; break;}
+            if(sc[j] == st[i+j] && sc[j]=='1' && tsc == 2 && st[i+j+1] =='0' && sc[j+1]=='0'){flag = 1; break;}
             else{
-                if(sc[j] == st[i+j]){flag = 1;} else {flag = 0; break;}
+                if(sc[j] == st[i+j] && tsc == 1 && sc[j] != '1'){flag = 1;} else {flag = 0; break;}
             }
         }
     }
@@ -67,20 +70,25 @@ int test_sous_chaine(char * sc, char * st){
  * représenté dans le tableau; si tous les noeuds sont présents 
  * dans le tableau elle retourne 0.
  */
-
 int check(void ** tab){
     int i/*boucle sur les noeuds*/, j/*boucle sur le tableau*/, flag = 1/*identifie la présence d'un noeud*/;
     liste * tmp = NULL;
     for(i=1; i <= NumberOfNodes && flag == 1; ++i){
-        for(j = 0; j<NumberOfNodes && flag == 0; ++j){
-            tmp = tab[i];
-            while(tmp != NULL && flag == 0){
-                flag = test_sous_chaine(intToString(i+1), intToString(tmp->value));
+        for(j = 0; j<NumberOfNodes; ++j){
+            tmp = tab[j];
+            while(tmp == NULL && j < NumberOfNodes){++j; tmp = tab[j];}
+            while(tmp != NULL){
+                flag = test_sous_chaine(intToString(i), intToString(tmp->value));
+                if(flag == 1){break;}
                 tmp = tmp->l;
-            }
+           }
+            if(flag == 1){break;}
         }
     }
-    if(flag == 0){printf("le sommet %i n'est pas représenté\n", i); return i;}
+    /*Après la 1ère itération, la boucle incrémente d'abord le i 
+     * avant de tester la condition de passage, raison pour laquelle on
+     * décremente ici, afin de récupérer la valeur de la dernière itération*/
+    if(flag == 0){--i; printf("le sommet %i n'est pas représenté\n", i); return i;}
     return 0;
 }
 
@@ -88,11 +96,19 @@ int check(void ** tab){
 void afficheTab(void ** tab){
     int i;
     for(i=0; i < NumberOfNodes; ++i){
-        if(tab[i] != NULL){afficheListe(tab[i]);printf("\n");}
+        if(tab[i] != NULL){printf("case %d: ", i);afficheListe(tab[i]);printf("\n");}
     }
 }
 
-int concateneEntier(int a1, int a2, int a3){ return ((a1*(10*calculTailleEntier(a2))+a2)*10*(calculTailleEntier(a3)))+a3; }
+int power(int base, int exposant){
+    int i, res = base;
+    for(i = 1; i < exposant; ++i){res *= res;}
+    return res;
+}
+
+int concateneEntier(int a1, int a2, int a3){
+    return ((a1*power(10,calculTailleEntier(a2))+a2)*power(10,calculTailleEntier(a3)))+a3;
+}
 
 /*prend une liste d'entier et renvoie la liste contenant 
  * tous les arrangements de toutes les valeurs de la liste
@@ -185,7 +201,7 @@ void showElt(void * pt){ printf(" %s ", (char*)pt); }
 
 int main(int argc, char ** argv){
     /*________________________________________vérification des entrées___________________________________*/
-    int i, j;
+    int i, j, res;
     liste * ret;
     if(argc == 2){
         if(sscanf(argv[1], "%d", &NumberOfNodes) != EOF){} 
@@ -205,8 +221,11 @@ int main(int argc, char ** argv){
             if(ret == NULL){ printf("on ne peut pas former une branche de somme %d avec %d comme valeur max\n", i, j); }
             else {printf("avec %d en partant de %d on obtient : ", i, j);afficheListe(ret); printf("\n");}
         }
-        printf("voici la liste des valeurs pour %d:\n", i);
-        generateAllValue(tab);
+        if((res = check(tab)) == 0){
+            printf("voici la liste des valeurs pour %d:\n", i);
+            generateAllValue(tab);
+        }
+        else{printf("la valeur %i n'a pas de solution avec le noeud %d\n", i, res);}
     }
     char * chaine1 = "test pour voir l'arithmétique des pointeurs";
     printf("initialement chaine1 = %s\n", chaine1);
